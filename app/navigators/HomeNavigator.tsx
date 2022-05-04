02202cb9f66e699b94f612c9bb11ce1367020594
keval <kg@pop-os.localdomain>
2022-05-04 19:14:01 +0530

    fullscreen added

--- app/navigators/HomeNavigator.tsx
+++ app/navigators/HomeNavigator.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Platform } from 'react-native';
 import Home from '@app/screens/Home/Scenes/Home';
@@ -10,6 +10,7 @@ import Chapters from '@app/screens/Home/Scenes/Chapters';
 import Videos from '@app/screens/Home/Scenes/Videos';
 import Subject from '@app/screens/Home/Scenes/Subject';
 import ContinueLearn from '@app/screens/Home/Scenes/ContinueLearn';
+import { ScreenContainer } from 'react-native-video-extension';
 
 export type HomeNavigatorParamList = {
     Home: undefined;
@@ -20,68 +21,93 @@ export type HomeNavigatorParamList = {
 const Tab = createBottomTabNavigator<HomeNavigatorParamList>();
 
 export const HomeNavigator = () => {
+    const [showTabNavigator, setShowTabNavigator] = useState({});
+
     return (
-        <Tab.Navigator
-            screenOptions={() => ({
-                headerShown: false,
-                tabBarShowLabel: false,
-                tabBarStyle: {
-                    height: Platform.OS === 'android' ? 70 : 80,
-                    borderTopColor: 'transparent',
-                    backgroundColor: '#fff',
-                    shadowColor: '#000',
-                    shadowOffset: {
-                        width: 0,
-                        height: 4,
-                    },
-                    shadowOpacity: 0.32,
-                    shadowRadius: 5.46,
-                    elevation: 9,
-                },
-                tabBarItemStyle: {
-                    height: 60,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                },
-            })}
-            initialRouteName="Home"
-        >
-            <Tab.Screen
-                name="Home"
-                component={Home}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <VPHome style={[{ opacity: 0.6 }, focused && { opacity: 1 }]} width={26} height={26} />
-                    ),
-                }}
-            />
-            <Tab.Screen
-                name="Player"
-                component={Video}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <VPPlayer style={[{ opacity: 0.6 }, focused && { opacity: 1 }]} width={26} height={26} />
-                    ),
-                }}
-            />
-            <Tab.Screen
-                name="Favourites"
-                component={Favourites}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <VPFavourites style={[{ opacity: 0.6 }, focused && { opacity: 1 }]} width={30} height={30} />
-                    ),
-                }}
-            />
-            <Tab.Screen
-                name="Settings"
-                component={ContinueLearn}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <VPSettings style={[{ opacity: 0.6 }, focused && { opacity: 1 }]} width={22} height={22} />
-                    ),
-                }}
-            />
-        </Tab.Navigator>
+        <ScreenContainer>
+            {({ fullscreen }) => {
+                return (
+                    <Tab.Navigator
+                        screenOptions={() => ({
+                            headerShown: false,
+                            tabBarShowLabel: false,
+                            tabBarStyle: {
+                                height: Platform.OS === 'android' ? 70 : 80,
+                                borderTopColor: 'transparent',
+                                backgroundColor: '#fff',
+                                shadowColor: '#000',
+                                shadowOffset: {
+                                    width: 0,
+                                    height: 4,
+                                },
+                                shadowOpacity: 0.32,
+                                shadowRadius: 5.46,
+                                elevation: 9,
+                                ...showTabNavigator,
+                            },
+                            tabBarItemStyle: {
+                                height: 60,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            },
+                        })}
+                        initialRouteName="Home"
+                    >
+                        <Tab.Screen
+                            name="Home"
+                            component={Home}
+                            options={{
+                                tabBarIcon: ({ focused }) => (
+                                    <VPHome
+                                        style={[{ opacity: 0.6 }, focused && { opacity: 1 }]}
+                                        width={26}
+                                        height={26}
+                                    />
+                                ),
+                            }}
+                        />
+                        <Tab.Screen
+                            name="Player"
+                            children={() => <Video fullscreen={fullscreen} setShowTabNavigator={setShowTabNavigator} />}
+                            options={{
+                                tabBarIcon: ({ focused }) => (
+                                    <VPPlayer
+                                        style={[{ opacity: 0.6 }, focused && { opacity: 1 }]}
+                                        width={26}
+                                        height={26}
+                                    />
+                                ),
+                            }}
+                        />
+                        <Tab.Screen
+                            name="Favourites"
+                            component={Favourites}
+                            options={{
+                                tabBarIcon: ({ focused }) => (
+                                    <VPFavourites
+                                        style={[{ opacity: 0.6 }, focused && { opacity: 1 }]}
+                                        width={30}
+                                        height={30}
+                                    />
+                                ),
+                            }}
+                        />
+                        <Tab.Screen
+                            name="Settings"
+                            component={ContinueLearn}
+                            options={{
+                                tabBarIcon: ({ focused }) => (
+                                    <VPSettings
+                                        style={[{ opacity: 0.6 }, focused && { opacity: 1 }]}
+                                        width={22}
+                                        height={22}
+                                    />
+                                ),
+                            }}
+                        />
+                    </Tab.Navigator>
+                );
+            }}
+        </ScreenContainer>
     );
 };

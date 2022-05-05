5de1bd5736043366e1fe0b661741e1f1ce684ff1
keval <kg@pop-os.localdomain>
2022-05-05 16:23:31 +0530

    fullscreen fix

--- app/screens/Home/Scenes/Video.tsx
+++ app/screens/Home/Scenes/Video.tsx
@@ -11,10 +11,11 @@ interface VideoProps {
     setShowTabNavigator: () => void;
 }
 
-const Video: React.FC<VideoProps> = () => {
+const VideoScreen: React.FC<VideoProps> = (props) => {
+    const { fullscreen } = props;
+
     const VIDEO_URL = 'https://stream.mux.com/Tyu80069gbkJR2uIYlz2xARq8VOl4dLg3.m3u8';
     const [isPlayer, setIsPlayer] = useState(false);
-    const { fullscreen } = useVideoCtx();
 
     useEffect(() => {
         if (fullscreen) {
@@ -25,52 +26,56 @@ const Video: React.FC<VideoProps> = () => {
     }, [fullscreen]);
 
     return (
-        <ScreenContainer>
-            {({ fullscreen }) => (
-                <SafeAreaView style={STYLES.container}>
-                    <View style={STYLES.innerContainer}>
-                        {isPlayer ? (
-                            <View
-                                style={[
-                                    !fullscreen
-                                        ? { width: SIZES.width, height: SIZES.height * 0.25 }
-                                        : STYLES.playerContainer,
-                                    STYLES.defaultPlayerContainer,
-                                ]}
-                            >
-                                <VideoPlayer URL={VIDEO_URL} />
-                            </View>
-                        ) : (
-                            <View>
-                                <View style={STYLES.topHeaderContainer}>
-                                    <VideoPlayerBack height="40" width="40" />
-                                    <VideoPlayerLike height="40" width="40" />
-                                </View>
-                                <Image
-                                    style={{ width: SIZES.width, height: SIZES.height * 0.3 }}
-                                    source={require('../../../assets/images/VideoPlayerBG.png')}
-                                />
-                                <Pressable style={STYLES.playerButton} onPress={() => setIsPlayer(true)}>
-                                    <RKLogo height="80" width="80" />
-                                </Pressable>
-                            </View>
-                        )}
-
-                        {!fullscreen && (
-                            <View style={STYLES.lowerTextContainer}>
-                                <Text style={STYLES.primaryText}>Umang Gadhavana</Text>
-                                <Text style={STYLES.titleText}>Socket Io Basics</Text>
-                                <Text style={STYLES.primaryText}>Lenght: 1.5 hours</Text>
-                                <Text style={STYLES.descText}>
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                                    Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                                    unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                </Text>
-                            </View>
-                        )}
+        <SafeAreaView style={STYLES.container}>
+            <View style={STYLES.innerContainer}>
+                {isPlayer ? (
+                    <View
+                        style={[
+                            !fullscreen ? { width: SIZES.width, height: SIZES.height * 0.25 } : STYLES.playerContainer,
+                            STYLES.defaultPlayerContainer,
+                        ]}
+                    >
+                        <VideoPlayer URL={VIDEO_URL} fullscreen={fullscreen} />
                     </View>
-                </SafeAreaView>
-            )}
+                ) : (
+                    <View>
+                        <View style={STYLES.topHeaderContainer}>
+                            <VideoPlayerBack height="40" width="40" />
+                            <VideoPlayerLike height="40" width="40" />
+                        </View>
+                        <Image
+                            style={{ width: SIZES.width, height: SIZES.height * 0.3 }}
+                            source={require('../../../assets/images/VideoPlayerBG.png')}
+                        />
+                        <Pressable style={STYLES.playerButton} onPress={() => setIsPlayer(true)}>
+                            <RKLogo height="80" width="80" />
+                        </Pressable>
+                    </View>
+                )}
+
+                {!fullscreen && (
+                    <View style={STYLES.lowerTextContainer}>
+                        <Text style={STYLES.primaryText}>Umang Gadhavana</Text>
+                        <Text style={STYLES.titleText}>Socket Io Basics</Text>
+                        <Text style={STYLES.primaryText}>Lenght: 1.5 hours</Text>
+                        <Text style={STYLES.descText}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type specimen book.
+                        </Text>
+                    </View>
+                )}
+            </View>
+        </SafeAreaView>
+    );
+};
+
+const Video = () => {
+    return (
+        <ScreenContainer>
+            {({ fullscreen }) => {
+                return <VideoScreen fullscreen={fullscreen} />;
+            }}
         </ScreenContainer>
     );
 };
